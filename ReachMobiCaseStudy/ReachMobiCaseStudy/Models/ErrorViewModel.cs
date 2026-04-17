46c846ffd6f8b4e171b6be07315aa8448ebc81ef
seankeever <seankeever@gmail.com>
2026-04-16 20:40:43 -0400

    Blank .net Core MVC App

---
+++ ReachMobiCaseStudy/ReachMobiCaseStudy/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ReachMobiCaseStudy.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
